cf53c676206357596af605990fffffe912aac3ef
amasri88 <amran.man99@gmail.com>
2022-06-25 02:38:32 +0300

    initialize react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,23 @@
 import './App.css';
-import { Container, AppBar, Typography, Grid, Grow, } from '@mui/material';
-import Form from './components/Form/From';
-import Posts from './components/Posts/Posts';
-import memories from './images/memories.png';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getPostsAction } from './redux/postSlice';
-import axios from 'axios';
+import { Container } from '@mui/material';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const dispatch = useDispatch();
-  const { currentId } = useSelector((state => state.posts));
 
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const { data } = await axios.get('http://localhost:4000/api/v1/posts');
-        dispatch(getPostsAction(data));
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getPosts();
-
-  }, [currentId, dispatch]);
 
   return (
-    <div className="App">
-      <Container maxWidth='lg'>
-        <AppBar className='AppBar' position='static' color='inherit' >
-          <Typography className='Typography' variant='h2' align='center'> Memories</Typography>
-          <img src={memories} alt='meomories' className="logo" />
-        </AppBar>
-        <Grow in>
-          <Container>
-            <Grid className = 'grid-content' container spacing={3} >
-              <Grid item xs={12} sm={7}>
-                <Posts />
-              </Grid>
-              <Grid item xs={12} sm={5}>
-                <Form />
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
-      </Container>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Container maxWidth='lg'>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
