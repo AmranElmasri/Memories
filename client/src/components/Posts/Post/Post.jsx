679312e480667af7a75815f244cd174fb9bf8758
amasri88 <amran.man99@gmail.com>
2022-07-16 15:42:29 +0300

    fix

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -95,7 +95,7 @@ export default function Post({ post }) {
         <Typography className='title' gutterBottom variant="h5" component="h2">{post.title}</Typography>
 
         <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+          <Typography className='message' variant="body2" color="textSecondary" component="p">{post.message.split('').slice(0, 40).join('')} ...</Typography>
         </CardContent>
       </ButtonBase>
 
