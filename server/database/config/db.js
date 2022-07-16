372a6f56108806211a83af5d9fc4ee5d9f2d112f
amasri88 <amran.man99@gmail.com>
2022-07-16 04:51:52 +0300

    fix & deploy

--- server/database/config/db.js
+++ server/database/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.DB_URI);
+        const connection = await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
         console.log(`MongoDB Connected: ${connection.connection.host}`.cyan.underline)
 
